03b37e91eea77549e7ebaf391793625100b82a08
yangjiapei <1098486124@qq.com>
2021-11-05 11:21:02 +0800

    feat: aaa

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { render } from 'react-dom'
-import './index.scss'
-import aPng from './assets/images/02.jpeg'
+import React from 'react';
+import { render } from 'react-dom';
+import './index.scss';
+import aPng from './assets/images/02.jpeg';
 
 try {
-  const rootElement = document.getElementById('root')
-  console.log('运行')
-  const App = () => {
-    return <div className="hello">
+  const rootElement = document.getElementById('root');
+  console.log('运行1');
+  const App = () => (
+    <div className="hello">
       Hello1
-      <img src={aPng}/>
+      <img src={aPng} alt="" />
     </div>
-  }
-  render(<App/>, rootElement)
+  );
+  render(<App />, rootElement);
 } catch (err) {
-  console.log('err', err)
+  console.log('err', err);
 }
